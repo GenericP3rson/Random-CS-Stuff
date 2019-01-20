b2ab344de9945f0622ff362e3174274b246360e0
Paromita Datta <paromitadatta@Paromitas-MacBook-Pro.local>
2019-01-20 10:57:54 -0600

    C# Stuff...

---
+++ cringe.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+namespace Cringe {
+    public class Cringe{
+        public static void Main(string[] args){
+            Console.WriteLine("Does this work?");
+            Console.ReadKey();
+        }
+    }
+}
